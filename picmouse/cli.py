ab7c45c403bc8fac08a83a4cf5fc03bbe27dd059
Toni Sredanovic <toni.sredanovic@gmail.com>
2024-03-18 08:36:54 +0100

    refactoring and draw cmd start

--- picmouse/cli.py
+++ picmouse/cli.py
@@ -3,7 +3,76 @@ import click
 import pyautogui
 from PIL import Image
 
-from utils import convert
+from utils import load_image, save_image, convert
+
+# SHARED COMMAND OPTIONS
+
+_base_convert_command_options = [
+    click.option(
+        "--in-path", type=click.Path(), required=True, help="Input image path."
+    ),
+    click.option(
+        "--width",
+        type=click.INT,
+        help="Output image width. Defaults to input image width.",
+    ),
+    click.option(
+        "--height",
+        type=click.INT,
+        help="Output image height. Defaults to input image height.",
+    ),
+    click.option(
+        "--resample",
+        type=click.Choice(
+            ["nearest", "box", "bilinear", "hamming", "bicubic", "lanczos"],
+            case_sensitive=False,
+        ),
+        default="nearest",
+        help="Resampling used when resizing image. Only used if width and/or height is provided. Defaults to nearest.",
+    ),
+    click.option(
+        "--resolution",
+        type=click.IntRange(min=0, max=100),
+        default=100,
+        help="Output image resolution. Defaults to 100.",
+    ),
+    click.option(
+        "--threshold",
+        type=click.IntRange(min=0, max=255),
+        default=125,
+        help="Threshold used when converting to monochrome. Defaults to 125.",
+    ),
+    click.option(
+        "--invert", is_flag=True, show_default=True, default=False, help="Invert image."
+    ),
+]
+
+
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+
+    return _add_options
+
+
+# COMMAND ARGS MAPPINGS
+
+
+def get_resample(resample):
+    resample_map = {
+        "nearest": Image.Resampling.NEAREST,
+        "box": Image.Resampling.BOX,
+        "bilinear": Image.Resampling.BILINEAR,
+        "hamming": Image.Resampling.HAMMING,
+        "bicubic": Image.Resampling.BICUBIC,
+        "lanczos": Image.Resampling.LANCZOS,
+    }
+    return resample_map[resample]
+
+
+# COMMANDS
 
 
 @click.group()
@@ -11,52 +80,16 @@ def cli():
     pass
 
 
-@cli.command(name='convert')
-@click.option("--in-path", type=click.Path(), required=True, help="Input image path.")
+# Convert iamge command
+
+
+@cli.command(name="convert")
 @click.option("--out-path", type=click.Path(), required=True, help="Output image path.")
-@click.option(
-    "--width",
-    type=click.INT,
-    help="Output image width. Defaults to input image width.",
-)
-@click.option(
-    "--height",
-    type=click.INT,
-    help="Output image height. Defaults to input image height.",
-)
-@click.option(
-    "--resample",
-    type=click.Choice(
-        ["nearest", "box", "bilinear", "hamming", "bicubic", "lanczos"],
-        case_sensitive=False,
-    ),
-    default="nearest",
-    help="Resampling used when resizing image. Only used if width and/or height is provided. Defaults to nearest.",
-)
-@click.option(
-    "--resolution",
-    type=click.IntRange(min=0, max=100),
-    default=100,
-    help="Output image resolution. Defaults to 100.",
-)
-@click.option(
-    "--threshold",
-    type=click.IntRange(min=0, max=255),
-    default=125,
-    help="Threshold used when converting to monochrome. Defaults to 125.",
-)
-@click.option(
-    "--invert", is_flag=True, show_default=True, default=False, help="Invert image."
-)
-def convert_command(in_path, out_path, width, height, resample, resolution, threshold, invert):
-    resample_map = {
-        "nearest": Image.Resampling.NEAREST,
-        "box": Image.Resampling.BOX,
-        "bilinear": Image.Resampling.BILINEAR,
-        "hamming": Image.Resampling.HAMMING,
-        "bicubic": Image.Resampling.BICUBIC,
-    }
-    resample = resample_map[resample]
+@add_options(_base_convert_command_options)
+def convert_command(
+    in_path, out_path, width, height, resample, resolution, threshold, invert
+):
+    resample = get_resample(resample)
     click.echo(f"in_path: {in_path}")
     click.echo(f"out_path: {out_path}")
     click.echo(f"width: {width}")
@@ -66,20 +99,80 @@ def convert_command(in_path, out_path, width, height, resample, resolution, thre
     click.echo(f"threshold: {threshold}")
     click.echo(f"invert: {invert}")
 
-    convert(in_path, out_path, width, height, resolution, threshold, resample, invert)
+    img = load_image(in_path)
+    img = convert(img, width, height, resolution, threshold, resample, invert)
+    save_image(img, out_path)
 
 
-@cli.command(name='mouse-to-pos')
-@click.option("--pos", nargs=2, type=click.INT, required=True, help="Moves mouse to this position.")
+# Draw image command
+
+
+@cli.command(name="draw")
+@click.option(
+    "--pos",
+    nargs=2,
+    type=click.INT,
+    required=True,
+    help="Starts drawing at this position.",
+)
+@add_options(_base_convert_command_options)
+def draw_command(pos, in_path, width, height, resample, resolution, threshold, invert):
+    resample = get_resample(resample)
+    click.echo(f"pos: {pos}")
+    click.echo(f"in_path: {in_path}")
+    click.echo(f"width: {width}")
+    click.echo(f"height: {height}")
+    click.echo(f"resample: {resample}")
+    click.echo(f"resolution: {resolution}")
+    click.echo(f"threshold: {threshold}")
+    click.echo(f"invert: {invert}")
+
+    img = load_image(in_path)
+    img = convert(img, width, height, resolution, threshold, resample, invert)
+
+    pyautogui.moveTo(pos[0], pos[1])
+
+
+# Move mouse to possition command
+
+
+@cli.command(name="mouse-to-pos")
+@click.option(
+    "--pos",
+    nargs=2,
+    type=click.INT,
+    required=True,
+    help="Moves mouse to this position.",
+)
 def mouse_to_pos_command(pos):
     click.echo(f"pos: {pos}")
-    x, y = pos
-    pyautogui.moveTo(x, y)
+    pyautogui.moveTo(pos[0], pos[1])
 
-@cli.command(name='mouse-start-to-finish')
-@click.option("--start", nargs=2, type=click.INT, required=True, help="Moves mouse from this position.")
-@click.option("--finish", nargs=2, type=click.INT, required=True, help="Moves mouse to this position.")
-@click.option("--seconds", type=click.INT, default=3, help="Time for movement and waiting between movements.")
+
+# Move mouse from start to finish position command
+
+
+@cli.command(name="mouse-start-to-finish")
+@click.option(
+    "--start",
+    nargs=2,
+    type=click.INT,
+    required=True,
+    help="Moves mouse from this position.",
+)
+@click.option(
+    "--finish",
+    nargs=2,
+    type=click.INT,
+    required=True,
+    help="Moves mouse to this position.",
+)
+@click.option(
+    "--seconds",
+    type=click.INT,
+    default=3,
+    help="Time for movement and waiting between movements.",
+)
 def mouse_to_pos_command(start, finish, seconds):
     click.echo(f"start: {start}")
     click.echo(f"finish: {finish}")
@@ -87,6 +180,7 @@ def mouse_to_pos_command(start, finish, seconds):
     pyautogui.moveTo(start[0], start[1], seconds)
     sleep(seconds)
     pyautogui.moveTo(finish[0], finish[1], seconds)
+
 
 if __name__ == "__main__":
     cli()
--- picmouse/utils.py
+++ picmouse/utils.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageOps
 
 def load_image(path):
     with Image.open(path) as in_img:
-        return in_img.convert('RGB')
+        return in_img.convert("RGB")
 
 
 def save_image(img, path):
@@ -16,39 +16,59 @@ def resize(img, width, height, resample):
 
 def apply_resolution(img, resolution, resample):
     width, height = img.size
-    img = img.resize((width * resolution // 100, height * resolution // 100)) # TODO add resample here
+    img = img.resize(
+        (width * resolution // 100, height * resolution // 100)
+    )  # TODO add resample here
     return img.resize((width, height), resample)
 
 
 def apply_threshold(img, threshold):
-    img = img.convert('L')
-    img = img.point( lambda p: 255 if p > threshold else 0 )
-    return img.convert('1')
+    img = img.convert("L")
+    img = img.point(lambda p: 255 if p > threshold else 0)
+    return img.convert("1")
 
 
 def invert(img):
     return ImageOps.invert(img)
 
-def convert(in_path, out_path, width=None, height=None, resolution=None, threshold=None, resample=None, invert=False):
-    # Load image
-    img = load_image(in_path)
 
+def convert(
+    img,
+    width=None,
+    height=None,
+    resolution=None,
+    threshold=None,
+    resample=None,
+    invert=False,
+):
     # Resize only if width and/or height are provided
     width = width if width else img.width
     height = height if height else img.height
     if width != img.width or height != img.height:
         img = resize(img, width, height, resample)
-    
+
     # Apply resolution if less than 100
     if resolution < 100:
         img = apply_resolution(img, resolution, resample)
-    
+
     # Apply threshold
     img = apply_threshold(img, threshold)
 
     # Invert if needed
     if invert:
         img = invert(img)
-    
-    # Save image
-    save_image(img, out_path)
+
+    return img
+
+
+def draw(
+    in_path,
+    pos,
+    width=None,
+    height=None,
+    resolution=None,
+    threshold=None,
+    resample=None,
+    invert=False,
+):
+    pass
